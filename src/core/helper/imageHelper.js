536d268360405b750c98835c0ecb3052fa0b0a3e
Godwin Ehikhamhen <gehikhamhen247@gmail.com>
2023-02-23 14:16:10 +0100

    done with frontend.

--- src/core/helper/imageHelper.js
+++ src/core/helper/imageHelper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function ImageHelper({product}) {
-    const imageUrl = product && product.image ? product.image : "./produc.jpg";
+    const imageUrl = product && product.image ? product.image : "./static/frontend/product.jpg";
     return (
         <div className='rounded border border-success p-2'>
             <img src={imageUrl} alt="" style={{maxHeight:"100%", maxWidth:"100%"}} className="mb-3 rounded"/>
